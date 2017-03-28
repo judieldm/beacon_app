c56005a4285b7a60192765f0c7c33481b982ad1c
judiel <judiel.reyes@codit.us>
2017-03-27 23:41:26 -0300

    adding crosswalk

--- app/main/services/data-service-serv.js
+++ app/main/services/data-service-serv.js
@@ -4,17 +4,20 @@
   .service('DataService', dataService);
   function dataService ($log, $cordovaSQLite) {
     var vm = this;
+    var isAndroid = ionic.Platform.isAndroid();
     vm.init = onInit;
     function onInit () {
       var db;
       if (window.cordova) {
-        db = $cordovaSQLite.openDB({ name: 'my.db', bgType: 1 });
-        var query = 'CREATE TABLE IF NOT EXISTS DEMO (id INTEGER PRIMARY KEY AUTOINCREMENT, name,number)';
-        $cordovaSQLite.execute(db, query).then(function (res) {
-          $log.log(res);
-        });
+        if (isAndroid) {
+          db = $cordovaSQLite.openDB({ name: 'my.db', bgType: 1, iosDatabaseLocation: 'default' });
+          var query = 'CREATE TABLE IF NOT EXISTS DEMO (id INTEGER PRIMARY KEY AUTOINCREMENT, name,number)';
+          $cordovaSQLite.execute(db, query).then(function (res) {
+            $log.log(res);
+          });
+        }
       } else {
-        db = window.openDatabase('my.db', '1', 'my', 1024 * 1024 * 100); // browser
+        db = window.openDatabase('my.db', '1', 'my', 1024 * 1024 * 100);
       }
     }
   }
